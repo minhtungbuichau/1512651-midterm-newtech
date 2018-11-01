aa96f5d028960b6d114a592eee1dc7f895723589
BUI CHAU Minh Tung <1512651@student.hcmus.edu.vn>
2018-11-01 19:34:07 +0700

    included auth by google (not css)

--- src/component/auth/signin.js
+++ src/component/auth/signin.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { GoogleLogin } from 'react-google-login-component';
 import firebase from 'firebase';
+// import '../../.././public/frontend/login.css';
 export default class Signin extends Component {
     constructor (props, context) {
         super(props, context);
@@ -26,13 +27,32 @@ export default class Signin extends Component {
     render() {
     return (
         <div>
-            <GoogleLogin 
-                        socialId="yourClientID"
-                        className="google-login"
-                        scope="profile"
-                        fetchBasicProfile={false}
-                        responseHandler={this.responseGoogle}
-                        buttonText="Login With Google"/>
+              <div className="container">
+                <div className="row">
+                  <div className="col-sm-4" />
+                  <div className="col-sm-4">
+                    <p> Course: New technology in Software Development</p>  <hr />
+                    <p /><h2>Bùi Châu Minh Tùng - 1512651</h2><p />
+                    <hr />
+                    <buttonn className="btn btn-danger">
+                      <GoogleLogin 
+                          onSuccess={this.responseGoogle}
+                          onFailure={this.responseGoogle}
+                          socialId="310198683728-fkbd6auo302gj5rfr9lqufapr16trmut.apps.googleusercontent.com"
+                          className="google-login"
+                          scope="profile"
+                          fetchBasicProfile={false}
+                          responseHandler={this.responseGoogle}
+                          buttonText="Login With Google"
+                        />
+                    </buttonn>
+                  </div>
+                  <div className="col-sm-4" />
+                </div>
+              </div>
+
+            
+           
       </div>
     )
   }
